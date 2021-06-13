f44d011e1bee06fb0052b857648531ab472c8c12
castorga <36180482+castorga@users.noreply.github.com>
2021-06-12 17:50:09 -0700

    Chain-player shift, camera system and projectiles
    Added a way to change focus between the player and the chain, projectile launcher and a new camera system

--- Assets/Ball/ControlJoint.cs
+++ Assets/Ball/ControlJoint.cs
@@ -5,30 +5,49 @@ using UnityEngine;
 public class ControlJoint : MonoBehaviour
 {
     public float speed = 2.0f;
+    private bool enabled;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("w"))
-        {
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        if(enabled) {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical   = Input.GetAxisRaw("Vertical");
+            Vector3 direction = new Vector3(horizontal,0f,vertical).normalized;
+            transform.Translate(direction * Time.deltaTime * speed);
+            /*
+            if(Input.GetKey("i"))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            }
+            else if(Input.GetKey("k"))
+            {
+                transform.Translate(Vector3.back * Time.deltaTime * speed);
+            }
+            if(Input.GetKey("j"))
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * speed);
+            }
+            else if(Input.GetKey("l"))
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * speed);
+            }
+            if(Input.GetKey("o")) {
+                transform.Translate(Vector3.up * Time.deltaTime * speed);
+            }
+            else if(Input.GetKey("p")) {
+                transform.Translate(Vector3.down * Time.deltaTime * speed);
+            }
+            */
         }
-        if(Input.GetKey("s"))
-        {
-            transform.Translate(Vector3.back * Time.deltaTime * speed);
-        }
-        if(Input.GetKey("a"))
-        {
-            transform.Translate(Vector3.left * Time.deltaTime * speed);
-        }
-        if(Input.GetKey("d"))
-        {
-            transform.Translate(Vector3.right * Time.deltaTime * speed);
-        }
+    }
+
+    public void toggleEnabled() {
+        enabled = !enabled;
     }
 }
